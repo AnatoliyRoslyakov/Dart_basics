bcd7efbca122bfd52e2b7e7eaf5c516da0ba0308
Rosl9kov <rosliakov.tolik@gmail.com>
2022-11-22 22:32:20 +0300

    Добавил задание номер 5

--- dart_application_1/bin/5.dart
+++ dart_application_1/bin/5.dart
@@ -1,1 +1,26 @@
+void main(List<String> args) {
+  var wordsList = ['a', 'b', 'a', 'a', 'one', 'one', 'one', 'three', 'six'];
+  Set<int> findNumNamesIn(List<String> stringList) {
+    const intNames = {
+      'one': 1,
+      'two': 2,
+      'three': 3,
+      'four': 4,
+      'five': 5,
+      'six': 6,
+      'seven': 7,
+      'eight': 8,
+      'nine': 9
+    };
 
+    final set = <int>{};
+    for (var word in stringList) {
+      if (intNames[word] != null) {
+        set.add(intNames[word]!);
+      }
+    }
+    return set;
+  }
+
+  print('Цифры без повторений в коллекции: ${findNumNamesIn(wordsList)}');
+}
