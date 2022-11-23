010e409190bc1708207dfa670574b23129adb1ca
Rosl9kov <rosliakov.tolik@gmail.com>
2022-11-23 19:05:26 +0300

    Выполнил задачу 7

---
+++ dart_application_1/bin/7.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+  8.SqrtMetod(3); // <-- Корень  3-ей степени по основанию 8
+}
+
+extension on num {
+  num abs(num n) {
+    return n < 0 ? -n : n;
+  }
+
+  num? SqrtMetod(num rootDegree) {
+    num eps = 0.00000001; //допустимая погрешность
+    if (rootDegree == 0) {
+      print('Невозможно вычислить корень данной степени');
+    }
+    num root = this / rootDegree; //начальное приближение корня
+    num rn = this; //значение корня последовательным делением
+    int countiter = 0; //число итераций
+
+    while (abs(root - rn) >= eps) {
+      rn = this;
+      for (int i = 1; i < rootDegree; i++) {
+        rn = rn / root;
+      }
+      root = 0.5 * (rn + root);
+      countiter++;
+    }
+    print('Корень $rootDegree-степени  по основанию $this = $root');
+  }
+}
